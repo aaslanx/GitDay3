f67380e869162557999dda11cadf40e1bde98f45
CybertekSchool <github@cybertekschool.com>
2019-02-08 20:35:18 -0500

    add HashMap for fun

--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -11,12 +11,15 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
 //Login tests are maintained here.
 //without Page object approach
 public class LoginTests {
 
     WebDriver driver;
+    Map<String,String> myMap = new HashMap<String,String>();
 
     @BeforeMethod
     public void setUp() {
