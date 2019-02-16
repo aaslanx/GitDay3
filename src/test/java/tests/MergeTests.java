195bd6ec3d47fc9f266915568692f8cd1ea1d7f1
aaslanx <aaslanx@gmail.com>
2019-02-15 20:21:32 -0500

    method added to merge class

--- src/test/java/tests/MergeTests.java
+++ src/test/java/tests/MergeTests.java
@@ -1,4 +1,12 @@
 package tests;
 
+import org.testng.annotations.Test;
+
 public class MergeTests {
+
+
+    @Test
+    public void mergeTest1(){
+        System.out.println("Testing the merge");
+    }
 }
